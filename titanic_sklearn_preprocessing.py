db0f7305a64895cd9233e2a0f50525b33db211ca
σ`∀´)σ <hitdoomed@gmail.com>
2020-04-05 17:35:49 +0800

    Create titanic_sklearn_preprocessing.py

---
+++ titanic_sklearn_preprocessing.py
@@ -0,0 +1,43 @@
+import numpy
+import pandas as pd
+filepath="data/titanic3.xls"
+all_df = pd.read_excel(filepath)
+cols=['survived','name','pclass','sex','age','sibsp','parch','fare','embarked']
+all_df=all_df[cols]
+df = all_df.drop(['name'],axis=1)
+age_mean = df['age'].mean() #average null age
+df['age'] = df['age'].fillna(age_mean) #fill the null
+fare_mean = df['fare'].mean() #average null fare
+df['fare'] = df['fare'].fillna(fare_mean) #fill the null
+df['sex'] = df['sex'].map({'female':0,'male':1}).astype(int)
+x_OneHot_df = pd.get_dummies(data=df,columns=["embarked"]) #embarked classification convert
+ndarray = x_OneHot_df.values #dataframe convert array
+Label=ndarray[:0] #:=all 0=number 0 data field
+Features=ndarray[:,1:] #:=all 1:=number 1 data field to the last
+
+from sklearn import preprocessing
+minmax_scale = preprocessing.MinMaxScaler(feature_range=(0, 1))
+scaledFeatures=minmax_scale.fit_transform(Features)
+msk = numpy.random.rand(len(all_df)) < 0.8
+train_df = all_df[msk]
+test_df = all_df[~msk]
+def PreprocessData(raw_df):
+    df=raw_df.drop(['name'], axis=1)
+    age_mean = df['age'].mean()
+    df['age'] = df['age'].fillna(age_mean)
+    fare_mean = df['fare'].mean()
+    df['fare'] = df['fare'].fillna(fare_mean)
+    df['sex']= df['sex'].map({'female':0, 'male': 1}).astype(int)
+    x_OneHot_df = pd.get_dummies(data=df,columns=["embarked" ])
+
+    ndarray = x_OneHot_df.values
+    Features = ndarray[:,1:]
+    Label = ndarray[:,0]
+
+    minmax_scale = preprocessing.MinMaxScaler(feature_range=(0, 1))
+    scaledFeatures=minmax_scale.fit_transform(Features)    
+    
+    return scaledFeatures,Label
+
+
+
